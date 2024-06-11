276844adcc0c21705f0d2a66641f0d4852529608
sai kumar kusangi <saikumarkusangi007@gmail.com>
2024-06-11 17:19:24 +0530

    changed splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,9 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
 import 'package:onpods/providers/local_downloads_provider.dart';
 import 'package:onpods/utils/exports.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,15 +15,25 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(milliseconds: 500), () async {
+    _checkNetworkAndNavigate();
+  }
+
+  Future<void> _checkNetworkAndNavigate() async {
+    final connectivityResult = await Connectivity().checkConnectivity();
+
+    if (connectivityResult == ConnectivityResult.none) {
+      // No internet connection
+      Get.offAll(() => const NoConnection(), transition: Transition.fadeIn);
+    } else {
+      // Internet connection available
       final userId = await UserSession.getUserId();
-     
+
       if (userId != null) {
         Get.offAll(() => const Layout(), transition: Transition.fadeIn);
       } else {
         Get.offAll(() => const LoginScreen(), transition: Transition.fadeIn);
       }
-    });
+    }
   }
 
   @override
@@ -31,10 +45,11 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-          child: SizedBox(
-        height: MediaQuery.of(context).size.height / 3,
-        child: Image.asset(splashLogo),
-      )),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height / 3,
+          child: Image.asset(splashLogo),
+        ),
+      ),
     );
   }
 }
